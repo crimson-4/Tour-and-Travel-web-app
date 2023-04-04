e7f9cb84f6bd9c881b89b568844b7538ea8767ab
Amarjeet <amarjeet@whitehatjr.com>
2023-04-04 22:57:42 +0530

    cyclic update

--- server.js
+++ server.js
@@ -14,22 +14,39 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    // console.log(con.connection);
-    console.log('DB success');
-  });
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     // console.log(con.connection);
+//     console.log('DB success');
+//   });
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 const port = process.env.PORT || 8000;
 //console.log(port);
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+const server = connectDB().then(() => {
+  return app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+  });
 });
 
 process.on('unhandledRejection', (err) => {
